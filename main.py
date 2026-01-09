0225ae7b7fb0158a88c83c544da2b663b3c017dd
Robert Sadlak <robert.sadlak@protonmail.com>
2026-01-09 08:49:06 +0400

    created a player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}!")
@@ -8,6 +9,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while SCREEN_WIDTH == 1280:
         log_state()
@@ -17,8 +23,10 @@ def main():
                 return
 
         screen.fill('black')
-        pygame.display.flip()
-
+        player.draw(screen)
+        pygame.display.flip()     
+        dt = clock.tick(60)/1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
